1d1e9ccd04fe8ed470526f7d93fa8caf55d4260a
Alexandre-Akira <akira.enjiu@gmail.com>
2022-08-28 12:20:36 -0300

    feat: add titles components

---
+++ src/components/atoms/Title/index.ts
@@ -0,0 +1,38 @@
+import { styled } from "@/styles/stitches.config";
+
+export const H1 = styled("h1", {
+  fontSize: 56,
+  lineHeight: "58px",
+  letterSpacing: 2,
+  color: "$grey_scale_slot1",
+});
+
+export const H2 = styled("h2", {
+  fontSize: 40,
+  lineHeight: "44px",
+  letterSpacing: 1.5,
+});
+
+export const H3 = styled("h3", {
+  fontSize: 32,
+  lineHeight: "36px",
+  letterSpacing: 1.15,
+});
+
+export const H4 = styled("h4", {
+  fontSize: 28,
+  lineHeight: "38px",
+  letterSpacing: 2,
+});
+
+export const H5 = styled("h5", {
+  fontSize: 24,
+  lineHeight: "33px",
+  letterSpacing: 1.7,
+});
+
+export const H6 = styled("h6", {
+  fontSize: 18,
+  lineHeight: "24px",
+  letterSpacing: 1.3,
+});
