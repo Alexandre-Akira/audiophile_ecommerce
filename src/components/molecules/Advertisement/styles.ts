8df28f2b6b528e46a887c1dae9d3f08d0750db35
Alexandre-Akira <akira.enjiu@gmail.com>
2022-08-28 12:21:25 -0300

    style: add advertisement styles

---
+++ src/components/molecules/Advertisement/styles.ts
@@ -0,0 +1,26 @@
+import { styled } from "@/styles/stitches.config";
+import { OverLineText, BodyText } from "@/components/atoms/Text";
+
+export const StyledContainer = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  overflow: "hidden",
+  height: 610,
+});
+
+export const StyledContentContainer = styled("div", {
+  marginRight: 52,
+});
+
+export const StyledOverlineText = styled(OverLineText, {
+  marginBottom: 24,
+});
+
+export const StyledBodyText = styled(BodyText, {
+  marginTop: 24,
+  marginBottom: 40,
+});
+
+export const StyledPictureContainer = styled("div", {
+  opacity: 0.6,
+});
