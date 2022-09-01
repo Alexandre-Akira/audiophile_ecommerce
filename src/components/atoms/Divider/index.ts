e891998e8516398bcdba465ac98496e5c06f353b
Alexandre-Akira <akira.enjiu@gmail.com>
2022-09-01 20:23:34 -0300

    refactor: add divider to atoms component

---
+++ src/components/atoms/Divider/index.ts
@@ -0,0 +1,8 @@
+import { styled } from '@/styles/stitches.config'
+
+export const Divider = styled('div', {
+  width: '100%',
+  height: 1,
+  backgroundColor: '$grey_scale_slot1',
+  opacity: 0.2
+})
