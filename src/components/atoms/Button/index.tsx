b2c20e66d0d973a5f02c520c8afd3d9bd2adfc2b
Alexandre-Akira <akira.enjiu@gmail.com>
2022-09-01 20:22:50 -0300

    refactor: atoms path were updated to increase readability

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,24 +1,24 @@
-import { ButtonPropsType } from "./types";
-import { StyledButton } from "./styles";
-import { Chevron } from "@/assets/Chevron";
+import { ButtonPropsType } from './types'
+import { StyledButton } from './styles'
+import { Chevron } from '@/assets/icons'
 
 const Button: ButtonPropsType = ({ children, variant, ...props }) => {
   switch (variant) {
-    case "third":
+    case 'third':
       return (
         <StyledButton variant={variant} {...props}>
           {children}
           <Chevron />
         </StyledButton>
-      );
+      )
 
     default:
       return (
         <StyledButton variant={variant} {...props}>
           {children}
         </StyledButton>
-      );
+      )
   }
-};
+}
 
-export default Button;
+export default Button
---
+++ src/components/atoms/index.ts
@@ -0,0 +1,4 @@
+export * from './Button'
+export * from './Divider'
+export * from './Text'
+export * from './Title'
