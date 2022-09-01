63d7ee966e0d57238164da33668ad331394a2e50
Alexandre-Akira <akira.enjiu@gmail.com>
2022-09-01 20:26:37 -0300

    refactor: paths were updated

--- src/components/molecules/Advertisement/index.tsx
+++ src/components/molecules/Advertisement/index.tsx
@@ -1,14 +1,13 @@
-import Button from "@/components/atoms/Button";
-import * as React from "react";
-import { H1 } from "@/components/atoms/Title";
+import Button from '@/components/atoms/Button'
+import { H1 } from '@/components/atoms/Title'
+import * as React from 'react'
 import {
   StyledBodyText,
   StyledContainer,
-  StyledContentContainer,
   StyledOverlineText,
-  StyledPictureContainer,
-} from "./styles";
-import { HeadphoneXX99 } from "@/assets/products/HeadphoneXX99";
+  StyledPictureContainer
+} from './styles'
+import { HeadphoneXX99 } from '@/assets/products/HeadphoneXX99'
 
 interface IAdvertisementProps {}
 
@@ -17,18 +16,18 @@ export const Advertisement: React.FunctionComponent<IAdvertisementProps> = (
 ) => {
   return (
     <StyledContainer>
-      <StyledContentContainer>
+      <div>
         <StyledOverlineText>New Product</StyledOverlineText>
         <H1>XX99 Mark II Headphones</H1>
         <StyledBodyText variant="white">
           Experience natural, lifelike audio and exceptional build quality made
           for the passionate music enthusiast.
         </StyledBodyText>
-        <Button variant="orange">{"SEE PRODUCT" as const}</Button>
-      </StyledContentContainer>
+        <Button variant="orange">{'SEE PRODUCT' as const}</Button>
+      </div>
       <StyledPictureContainer>
         <HeadphoneXX99 />
       </StyledPictureContainer>
     </StyledContainer>
-  );
-};
+  )
+}
